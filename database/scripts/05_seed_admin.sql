1492a4475ce25b5d0f5518fb28871b8f67459d72
Ricardo Mauricio Apaza <ricardo.mauricio.apaza@alumnos.uta.cl>
2026-06-08 00:50:40 -0400

    Fix: corrección de puertos en proxy de Vite y configuración local

--- database/scripts/05_seed_admin.sql
+++ database/scripts/05_seed_admin.sql
@@ -1,2 +1,4 @@
 INSERT INTO usuarios (nombre, usuario, email, password_hash, rol)
-VALUES ('Administrador', 'admin', 'admin@tienda.cl', '$2b$10$e6d6kKYW.mdb0tScuEE6O.NG4RzlYQoMK3HhtcRqcoZjOSiIUZlzC', 'admin');
+VALUES 
+    ('Administrador', 'admin', 'admin@tienda.cl', '$2b$10$S3CcLZn3es4IHxq/gmbyseqW13o.qQOJTN0v1NbhG/A5eqyO20n8e', 'admin'),
+    ('Usuario Test', 'test', 'test@tienda.cl', '$2b$10$xw2KHwuEY6MVxIY2CqTXme0qxC2KsDChmrPlZS1MMkbfZtXOeZSLG', 'cliente');
